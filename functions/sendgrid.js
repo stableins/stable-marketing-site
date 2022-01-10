f73999974712142c1590e685a824293b0a6d2cdc
Josh <joshrbarrow@gmail.com>
2022-01-10 09:11:09 -0800

    Update sendgrid.js

--- functions/sendgrid.js
+++ functions/sendgrid.js
@@ -1,24 +1,31 @@
 const axios = require("axios")
 
 exports.handler = async function (event, context, callback) {
-  console.log(context)
-
-  // const createContact = emailInputValue => {
-  //   return axios.put("https://api.sendgrid.com/v3/marketing/contacts", {
-  //     contacts: [
-  //       {
-  //         email: emailInputValue,
-  //         custom_fields: {
-  //           w1_T: "email received",
-  //         },
-  //       },
-  //     ],
-  //     headers: {
-  //       Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
-  //     },
-  //   })
-  // }
-  return {
-    statusCode: 202,
+  try {
+    const response = await axios.put(
+      "https://api.sendgrid.com/v3/marketing/contacts",
+      {
+        contacts: [
+          {
+            email: emailInputValue,
+            custom_fields: {
+              w1_T: "email received",
+            },
+          },
+        ],
+        headers: {
+          Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
+        },
+      }
+    )
+    return {
+      statusCode: 202,
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      statusCode: 500,
+      message: e.message,
+    }
   }
 }
