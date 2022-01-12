23e18948ad3a46c28c8b7acaa5209fffaa1e92b7
Josh <joshrbarrow@gmail.com>
2022-01-12 13:23:48 -0800

    Update sendgridEmail.js

--- functions/sendgridEmail.js
+++ functions/sendgridEmail.js
@@ -1,48 +1,76 @@
-const axios = require("axios")
+const client = require("@sendgrid/mail")
+const { SENDGRID_API_KEY, } = process.env
 
 exports.handler = async function (event, context, callback) {
-  console.log("hi")
+  const { message, email, name } = JSON.parse(event.body)
+  client.setApiKey(SENDGRID_API_KEY)
+
+  const data = {
+    to: 'josh@stableins.com',
+    from: email,
+    subject: `New message from ${senderName} (${email})`,
+    html: message,
+  }
 
   try {
-    const body = JSON.parse(event.body)
-    const response = await axios.post(
-      "https://api.sendgrid.com/v3/mail/send",
-      {
-        personalizations: [
-          {
-            to: [
-              {
-                email: "josh@stableins.com",
-              },
-            ],
-            from: {
-              email: body.email,
-            },
-            subject: "Sending with SendGrid is Fun",
-          },
-          content: [{
-            type: "text/plain",
-            value: `Hi, my name is ${body.name}. ${body.message}`
-          }]
-        ],
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
-          "Content-Type": "application/json",
-        },
-      }
-    )
+    await client.send(data)
     return {
-      statusCode: 202,
+      statusCode: 200,
+      body: "Message sent",
     }
-  } catch (e) {
-    for (const error of e.response.data.errors) {
-      console.log(error)
-    }
+  } catch (err) {
     return {
-      statusCode: 500,
-      message: e.message,
+      statusCode: err.code,
+      body: JSON.stringify({ msg: err.message }),
     }
   }
 }
+
+// const axios = require("axios")
+
+// exports.handler = async function (event, context, callback) {
+//   console.log("hi")
+
+//   try {
+//     const body = JSON.parse(event.body)
+//     const response = await axios.post(
+//       "https://api.sendgrid.com/v3/mail/send",
+//       {
+//         personalizations: [
+//           {
+//             to: [
+//               {
+//                 email: "josh@stableins.com",
+//               },
+//             ],
+//             from: {
+//           },
+//               email: body.email,
+//             },
+//             subject: "Sending with SendGrid is Fun",
+//           content: [{
+//             type: "text/plain",
+//             value: `Hi, my name is ${body.name}. ${body.message}`
+//           }]
+//         ],
+//       },
+//       {
+//         headers: {
+//           Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
+//           "Content-Type": "application/json",
+//         },
+//       }
+//     )
+//     return {
+//       statusCode: 202,
+//     }
+//   } catch (e) {
+//     for (const error of e.response.data.errors) {
+//       console.log(error)
+//     }
+//     return {
+//       statusCode: 500,
+//       message: e.message,
+//     }
+//   }
+// }
