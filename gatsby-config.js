dacdc1c44fbca04cbde2e7a126d622760bb0407d
Josh <joshrbarrow@gmail.com>
2022-01-22 09:57:34 -0800

    add ms clarity

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 const path = require(`path`)
 
 require("dotenv").config({
-  path: '.env.${process.env.NODE_ENV}',
+  path: ".env.${process.env.NODE_ENV}",
 })
 
 module.exports = {
@@ -15,7 +15,7 @@ module.exports = {
   //   DEV_SSR: true,
   //   FAST_REFRESH: true,
   // },
-  plugins: [  
+  plugins: [
     `gatsby-plugin-styled-components`,
     `babel-plugin-styled-components`,
     {
@@ -24,13 +24,20 @@ module.exports = {
         implementation: require("sass"),
       },
     },
-      {
+    {
+      resolve: `gatsby-plugin-clarity`,
+      options: {
+        clarity_project_id: "aaujprt08a",
+        // Boolean value for enabling clarity while developing
+        // true will enable clarity tracking code on both development and production environments
+        // false will enable clarity tracking code on production environment only
+        enable_on_dev_env: true,
+      },
+    },
+    {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingIds: [
-          "UA-118217398-1",
-          "G-W6SQ1VMHXW",
-        ],
+        trackingIds: ["UA-118217398-1", "G-W6SQ1VMHXW"],
         pluginConfig: {
           head: true,
         },
