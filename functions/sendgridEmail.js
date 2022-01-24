c0cb01a0528932300289d08c7898b446588f5ad7
Josh <joshrbarrow@gmail.com>
2022-01-24 14:00:35 -0800

    Revert "add email validation api"
    This reverts commit 52e87fc1631ac2861011c0fc44e9c514eacddad4.

--- functions/sendgridEmail.js
+++ functions/sendgridEmail.js
@@ -2,51 +2,25 @@ const client = require("@sendgrid/mail")
 
 exports.handler = async function (event, context, callback) {
   const { message, email, name } = JSON.parse(event.body)
+  client.setApiKey(process.env.SENDGRID_API_KEY)
 
-  client.setApiKey(process.env.SENDGRID_EMAIL_VALIDATION)
-
-  const validateData = {
-    email: email,
-    source: "contact form",
+  const data = {
+    to: "josh@stableins.com",
+    from: "info@stableins.com",
+    subject: `New message from ${name} (${email})`,
+    html: message,
   }
 
-  const request = {
-    url: `/v3/validations/email`,
-    method: "POST",
-    body: validateData,
-  }
-
-  client
-    .request(request)
-    .then(([response, body]) => {
-      console.log(response.statusCode)
-      console.log(response.body)
-    })
-    .catch(error => {
-      console.error(error)
-    })
-
-  if (response.result.verdict === "Valid") {
-    client.setApiKey(process.env.SENDGRID_API_KEY)
-
-    const data = {
-      to: "josh@stableins.com",
-      from: "info@stableins.com",
-      subject: `New message from ${name} (${email})`,
-      html: message,
+  try {
+    await client.send(data)
+    return {
+      statusCode: 200,
+      body: "Message sent",
     }
-
-    try {
-      await client.send(data)
-      return {
-        statusCode: 200,
-        body: "Message sent",
-      }
-    } catch (err) {
-      return {
-        statusCode: 500,
-        body: err,
-      }
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: err,
     }
   }
 }
