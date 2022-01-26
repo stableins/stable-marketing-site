d66500b9abaac412dae3148555f59b54bd5337c3
EvanLundell <evan@stableins.com>
2022-01-26 13:30:45 -0600

    added Zoho integration and session info

---
+++ functions/saveSessionInfo.js
@@ -0,0 +1,28 @@
+const { MongoClient } = require("mongodb")
+
+exports.handler = async (event, context, callback) => {
+  const { session } = JSON.parse(event.body)
+  const uri = process.env.MONGO_URI.replace(
+    "<password>",
+    process.env.MONGO_PASSWORD
+  )
+  const client = new MongoClient(uri)
+  const statusCode = 200
+  const status = "Successfully inserted session info"
+
+  try {
+    await client.connect()
+    const database = client.db("marketing")
+    const sessions = database.collection("sessions")
+    await sessions.insertOne(session)
+  } catch (e) {
+    statusCode = 500
+    status = e.message
+    console.error(e)
+  } finally {
+    return {
+      statusCode,
+      body: JSON.stringify({ status })
+    }
+  }
+}
