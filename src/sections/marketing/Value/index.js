81e9fa6ad56885dd07a786106f78dbf3d1c49416
Josh <joshrbarrow@gmail.com>
2022-01-28 15:46:34 -0800

    Revert "Update index.js"
    This reverts commit d41b95dd21e060ac5625d65ecf7b5d5f0a3bf075.

--- src/sections/marketing/Value/index.js
+++ src/sections/marketing/Value/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import {
   Col,
   Container,
@@ -34,7 +34,6 @@ const FeatureSection = ({ ...rest }) => {
   const [statusResponse, setStatusResponse] = useState("")
   const [emailInputValue, setEmailInputValue] = useState("")
   const [loading, setLoading] = useState(false)
-  const [hasMounted, setHasMounted] = useState(null)
   const [color, setColor] = useState("#3b358a;")
   console.log(emailInputValue)
 
@@ -87,14 +86,6 @@ const FeatureSection = ({ ...rest }) => {
     }
   }
 
-  useEffect(() => {
-    setHasMounted(true)
-  }, [])
-
-  if (!hasMounted) {
-    return null
-  }
-
   if (formRedirect) {
     return <Redirect noThrow to="/join-stable" />
   }
@@ -107,7 +98,7 @@ const FeatureSection = ({ ...rest }) => {
       <Feature className="bg-blue-ribbon">
         <div className="inner-wrapper">
           <Container>
-            <Slide up>
+            <Slide left>
               <Row>
                 <Col xs="auto" className="col-xl-8 col-lg-10">
                   <div className="title">Get Your Free Driver Report </div>
