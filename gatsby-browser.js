8f90b2ee7185575ff9fa047314c267a6b3705f49
Josh <joshrbarrow@gmail.com>
2022-01-31 17:04:19 -0800

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,7 @@ import "./node_modules/slick-carousel/slick/slick.css"
 import "./node_modules/react-modal-video/css/modal-video.min.css"
 import "./src/assets/fonts/fontawesome-5/css/all.min.css"
 import { Layout } from "./src/components/Core"
+import { globalHistory } from "@reach/router"
 import { GlobalHeaderProvider } from "./src/context/GlobalHeaderContext"
 import { Provider } from "react-redux"
 import store from "./src/store"
@@ -24,4 +25,14 @@ export const constonInitialClientRender = () => {
     "popstate",
     () => (window.location.href = window.location.href)
   )
+}
+
+
+export const onInitialClientRender = () => {
+  /**
+   * This is a workaround for a bug in Gatsby
+   *
+   * See https://github.com/gatsbyjs/gatsby/issues/8357 for more details
+   */
+  globalHistory._onTransitionComplete()
 }
