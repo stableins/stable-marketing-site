5b9b1350a785334bb7306a27d2414a6d92de8beb
Josh <joshrbarrow@gmail.com>
2022-02-04 11:07:40 -0800

    optimization and initial blog set up

--- src/sections/pricing-2/index.js
+++
@@ -1,4 +0,0 @@
-// export { default as FaqSection } from "./Faq";
-// export { default as PricingSection } from "./Pricing";
-// export { default as InnerShape } from "./InnerPageShapeOne";
-// export { default as FooterSection } from "./Footer";
