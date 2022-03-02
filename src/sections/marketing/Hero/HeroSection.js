4223de7ecd7d6ea9884c18e9527bdcef1d3f00fc
Josh <joshrbarrow@gmail.com>
2022-03-02 15:36:43 -0800

    Update HeroSection.js

--- src/sections/marketing/Hero/HeroSection.js
+++ src/sections/marketing/Hero/HeroSection.js
@@ -147,7 +147,7 @@ export default function HeroSection() {
           payload: false,
         })
 
-        if (response.data.confirmed !== null {
+        if (response.data.confirmed !== null) {
           setShowExistingEmailModal(true)
         } else {
           navigate("/join-stable/")
